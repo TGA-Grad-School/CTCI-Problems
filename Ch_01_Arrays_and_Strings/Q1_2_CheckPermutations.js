41a73314160466f8330d02db62895fa66cdd524d
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2017-05-06 02:43:30 -0700

    updates

--- Ch_01_Arrays_and_Strings/Q1_2_CheckPermutations.js
+++ Ch_01_Arrays_and_Strings/Q1_2_CheckPermutations.js
@@ -1,15 +1,17 @@
 /* Check Permutation: Given two strings, write a method to decide if one is a permutation of the other. */
 
 // Test cases
-// 'cat dog' : 'gcat do', 'Taco truck' : 'uckTaco tr'
+// 'cat dog' : 'gcat do', 'cat dog' : 'gcadasat do'
+// 'Taco truck53' : 'uck5Taco t3r', 'Taco truck53' : 'uck534Taco t3r'
+
+// Split, sort and reduce into a single string value
 const sortAndReduce = (str = '') => {
-  str.split('')
-    .sort()
-    .reduce((acc, curr) => `${acc}${curr}`)[0];
+  return str.split('').sort()
+    .reduce((acc, curr) => acc + curr);
 };
 
 const checkPermutations = (str1 = '', str2 = '') => {
   return sortAndReduce(str1) === sortAndReduce(str2);
 };
 
-console.log(checkPermutations('cat dog', 'gcat do'));
+console.log(checkPermutations('Taco truck53', 'uck534Taco t3r'));
